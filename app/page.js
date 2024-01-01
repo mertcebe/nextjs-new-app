90e09cf76c53f6b1c1936ac54a3e6a08d3ef3226
mertcebe <mr.mertcebe@gmail.com>
2024-01-01 15:19:36 +0300

    study some lessons

--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
+"use client";
 import Link from 'next/link'
 import styles from './page.module.css'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
+  let router = useRouter();
+  const handleClick = (href) => {
+    router.push(href);
+  }
+  console.log('qwdqwd')
   return (
     <main className={styles.main}>
       <div>
         main page
-        <Link href={'/fetch-dynamic'}>fetchDynamic</Link>
+        <button onClick={() => { handleClick('/users') }}>go to users</button>
+        {/* <Link href={'/fetch-dynamic'}>fetchDynamic</Link>
         <Link href={'/fetch-revalidate'}>fetchRevalidate</Link>
-        <Link href={'/fetch-static'}>fetchStatic</Link>
+        <Link href={'/fetch-static'}>fetchStatic</Link> */}
       </div>
     </main>
   )
